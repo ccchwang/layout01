47036b8b7ba4e2caece67b6e45e4e9c6fd5df108
Chloe Hwang <c.chloeh@gmail.com>
2017-04-20 00:57:26 -0400

    changed animation on hover

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,9 @@ export default function About () {
       </Col>
 
       <Col className="about-content" xs={12} md={8}>
+      <div className="try">
+      <p>SEE MORE</p>
+      </div>
         <p className="about-heading-text">Art Direction</p>
         <p className="about-text">Starting with basic websites several years ago I found myself working as an Art Director for complex projects with a holistic approach soon. Visually appealing designs, subtle details and brand guidelines combined to innovative interfaces across various touch points became my daily companion.</p>
         <p className="about-heading-text">Art Direction</p>
